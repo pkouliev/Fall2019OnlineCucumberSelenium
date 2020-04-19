db83dd855547cd74608b20b3547a8f297f952b8e
pkouliev <pkouliev@gmail.com>
2020-04-19 11:36:49 -0400

    Special Setup added

--- src/test/java/com/vytrack/step_definitions/Hooks.java
+++ src/test/java/com/vytrack/step_definitions/Hooks.java
@@ -1,5 +1,6 @@
 package com.vytrack.step_definitions;
 
+import com.vytrack.utilities.Driver;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 
@@ -7,12 +8,23 @@ public class Hooks {
 
     @Before
     public void setup() {
-
         System.out.println("Test setup");
+        Driver.getDriver().manage().window().maximize();
+    }
+
+    @Before("@driver")
+    public void specialSetup(){
+        System.out.println("Setup for driver");
+    }
+
+    @After("@driver")
+    public void specialTearDown() {
+        System.out.println("Tear down for driver only");
     }
 
     @After
     public void tearDown() {
         System.out.println("Test clean up");
+        Driver.closeDriver();
     }
 }
