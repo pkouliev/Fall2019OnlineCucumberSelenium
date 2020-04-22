6bd1df7d95ad954e0ea3de780fe7c6495cd4346b
pkouliev <pkouliev@gmail.com>
2020-04-22 11:04:07 -0400

    updated

--- src/test/java/com/vytrack/pages/AbstractPageBase.java
+++ src/test/java/com/vytrack/pages/AbstractPageBase.java
@@ -27,8 +27,16 @@ public abstract class AbstractPageBase {
     @FindBy(css = "#user-menu > a")
     protected WebElement currentUser;
 
+    @FindBy(css = "[class='btn-group pull-right'] > button")
+    protected WebElement saveAndClose;
+
     public AbstractPageBase() {
         PageFactory.initElements(driver, this);
+    }
+
+    public void clickOnSaveAndClose() {
+        wait.until(ExpectedConditions.elementToBeClickable(saveAndClose));
+        saveAndClose.click();
     }
 
     public String getCurrentUserName() {
