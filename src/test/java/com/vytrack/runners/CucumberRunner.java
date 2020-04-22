8557ce36b3ba432585f24a7b91b9dcccda13287e
pkouliev <pkouliev@gmail.com>
2020-04-22 11:02:53 -0400

    dryRun status changed

--- src/test/java/com/vytrack/runners/CucumberRunner.java
+++ src/test/java/com/vytrack/runners/CucumberRunner.java
@@ -10,7 +10,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         dryRun = false,
         strict = false,
-        tags = "@login",
+        tags = "@create_car",
         plugin = {
                 "html:target/default-report",
                 "json:target/cucumber1.json"
