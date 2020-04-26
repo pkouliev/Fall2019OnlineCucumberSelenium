461ec08a95a807472def9377ebda0913a064b117
pkouliev <pkouliev@gmail.com>
2020-04-26 16:44:55 -0400

    SmokeTestRunner added

---
+++ src/test/java/com/vytrack/runners/SmokeTestRunner.java
@@ -0,0 +1,22 @@
+package com.vytrack.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        glue = "com/vytrack/step_definitions",
+        features = "src/test/resources/features",
+        dryRun = false,
+        strict = false,
+        tags = "@smoke_test",
+        plugin = {
+                "html:target/smoke_test_default-report",
+                "json:target/cucumber1.json"
+        }
+)
+
+public class SmokeTestRunner {
+
+}
